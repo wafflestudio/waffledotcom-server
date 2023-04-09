05732fe2d0739fcb6f5ddbb7da7e101d9a29f1d9
serverrepairman <gustjwkd1007@gmail.com>
2023-04-09 18:26:24 +0900

    declare models

--- waffledotcom/src/database/models/user.py
+++ waffledotcom/src/database/models/user.py
@@ -1,4 +1,6 @@
 import sqlalchemy as sql
+import sqlalchemy_utils as sql_utils
+from sqlalchemy.orm import relationship
 
 from src.database.models import base as base_model
 
@@ -6,5 +8,33 @@ from src.database.models import base as base_model
 class User(base_model.DeclarativeBase):
     __tablename__ = "tb_user"
 
-    id = sql.Column(name="id", type_=sql.INT, primary_key=True, autoincrement=True)
-    name = sql.Column(name="name", type_=sql.VARCHAR(50))
+    u_idx = sql.Column(name="u_idx", type_=sql.INT, primary_key=True, autoincrement=True)
+    username = sql.Column(name="username", type_=sql.VARCHAR(50), unique=True)
+    password = sql.Column(name="password", type_=sql_utils.PasswordType(schemes=["pbkdf2_sha512"]))
+
+    first_name = sql.Column(name="first_name", type_=sql.VARCHAR(50))
+    last_name = sql.Column(name="last_name", type_=sql.VARCHAR(50))
+    student_id = sql.Column(name="student_id", type_=sql.VARCHAR(20))
+
+    department = sql.Column(name="department", type_=sql.VARCHAR(40))
+    college = sql.Column(name="college", type_=sql.VARCHAR(40))
+
+    phone_number = sql.Column(name="phone_number", type_=sql_utils.PhoneNumberType()) #+821012345678
+    github_id = sql.Column(name="github_id", type_=sql.VARCHAR(50))
+    github_email = sql.Column(name="github_email", type_=sql_utils.EmailType)
+    slack_id = sql.Column(name="slack_id", type_=sql.VARCHAR(50))
+    slack_email = sql.Column(name="slack_email", type_=sql_utils.EmailType)
+    notion_email = sql.Column(name="notion_email", type_=sql_utils.EmailType)
+    apple_email = sql.Column(name="apple_email", type_=sql_utils.EmailType, nullable=True)
+
+    generation = sql.Column(name="generation", type_=sql.INT)
+    active = sql.Column(name="active", type_=sql.BOOLEAN)
+
+    teams = relationship('TeamUserAssociation', back_populates='user')
+    positions = relationship('PositionUserAssociation', back_populates='user')
+    sns = relationship('SNS', back_populates='user')
+
+    introduce = sql.Column(name="introduce", type_=sql.TEXT, nullable=True)
+
+
+
