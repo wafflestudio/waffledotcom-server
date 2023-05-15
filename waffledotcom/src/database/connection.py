f9114638dafa382d5451e90e07c27b0879adce00
shinhong-park <shinhong.park@linecorp.com>
2023-05-15 23:48:30 +0900

    Rename `SQLAlchemyHandler` to `DBSessionFactory`, and simplify its implementation

--- waffledotcom/src/database/connection.py
+++ waffledotcom/src/database/connection.py
@@ -1,47 +1,32 @@
-import os
+from typing import Iterator
+from fastapi import Depends
 
 import sqlalchemy
-from sqlalchemy import engine
 from sqlalchemy import orm
 
 from waffledotcom.src.database.config import DBConfig
 from waffledotcom.src.utils.singleton import SingletonMeta
 
 
-class SQLAlchemyHandler(metaclass=singleton.SingletonMeta):
-    _engine: sqlalchemy.Engine | None
-    _session_factory: orm.sessionmaker | None
-
+class DBSessionFactory(metaclass=SingletonMeta):
     def __init__(self):
         self.engine: sqlalchemy.Engine = sqlalchemy.create_engine(DBConfig().url)
         self.session_maker = orm.sessionmaker(bind=self.engine, expire_on_commit=False)
 
-    @property
-    def engine(self) -> sqlalchemy.Engine:
-        if not self._engine:
-            raise RuntimeError("Engine is not created.")
-        return self._engine
+    def make_session(self) -> orm.Session:
+        session = self.session_maker()
+        return session
 
-    @property
-    def session_factory(self) -> orm.sessionmaker:
-        if not self._session_factory:
-            raise RuntimeError("Session factory is not created.")
-        return self._session_factory
-
-    def get_session(self) -> orm.Session:
-        session = self.session_factory()
-        try:
-            yield session
-        finally:
-            session.commit()
-            session.close()
-
-    def __del__(self):
-        if self._session_factory:
-            self._session_factory.close_all()
-        if self._engine:
-            self._engine.dispose()
+    def teardown(self):
+        orm.close_all_sessions()
+        self.engine.dispose()
 
 
-def get_db_connection():
-    return SQLAlchemyHandler()
+def get_db_session() -> Iterator[orm.Session]:
+    session = DBSessionFactory().make_session()
+
+    try:
+        yield session
+    finally:
+        session.commit()
+        session.close()
