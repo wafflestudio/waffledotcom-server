ec6f55b8445c9c7fe37f87ea0ac9862f0c54a36e
shinhong-park <shinhong.park@linecorp.com>
2023-06-18 00:44:59 +0900

    Implement external apis including slack & github(wip)

---
+++ waffledotcom/src/external/slack/config.py
@@ -0,0 +1,13 @@
+from pydantic import BaseSettings
+
+from waffledotcom.src.settings import settings
+
+
+class SlackConfig(BaseSettings):
+    api_token: str = ""
+
+    class Config:
+        case_sensitive = False
+        env_prefix = "SLACK_"
+
+        env_file = settings.env_files
