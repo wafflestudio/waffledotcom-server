f3a043d082a85fbb23203ea5b1b49ef4156dc8e8
shinhong-park <shinhong.park@linecorp.com>
2023-06-18 00:42:01 +0900

    Minor refactorings about env file management

--- waffledotcom/src/app.py
+++ waffledotcom/src/app.py
@@ -2,6 +2,7 @@ import fastapi
 
 from waffledotcom.src.apis.router import api_router
 from waffledotcom.src.database.connection import DBSessionFactory
+from waffledotcom.src.settings import settings
 
 
 def _add_routers(app: fastapi.FastAPI):
@@ -17,9 +18,7 @@ def _register_shutdown_event(app: fastapi.FastAPI):
 
 
 def create_app() -> fastapi.FastAPI:
-    app = fastapi.FastAPI(
-        title="waffledotcom-server",
-    )
+    app = fastapi.FastAPI(title="waffledotcom-server", debug=settings.is_dev)
     _add_routers(app)
     _register_shutdown_event(app)
     return app
