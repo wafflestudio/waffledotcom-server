903d47375fcafb94b6998111684aaa5df410a26c
Minkyu Lee <whizkyu@snu.ac.kr>
2023-09-24 03:23:33 +0900

    feat: 시크릿 매니저 세팅

--- waffledotcom/src/database/config.py
+++ waffledotcom/src/database/config.py
@@ -1,5 +1,8 @@
+from urllib.parse import quote_plus
+
 from pydantic import BaseSettings
 
+from waffledotcom.src.secrets import AWSSecretManager
 from waffledotcom.src.settings import settings
 
 
@@ -18,7 +21,19 @@ class DBConfig(BaseSettings):
 
     @property
     def url(self) -> str:
+        aws_secrets = AWSSecretManager()
+        if aws_secrets.is_available():
+            self.username = aws_secrets.get_secret("server_db_username")
+            self.password = quote_plus(aws_secrets.get_secret("server_db_password"))
+            self.host = aws_secrets.get_secret("server_db_host")
+            self.port = int(aws_secrets.get_secret("server_db_port"))
+            self.name = aws_secrets.get_secret("server_db_name")
+
         return (
             f"mysql+mysqldb://{self.username}:{self.password}"
             f"@{self.host}:{self.port}/{self.name}"
+            "?charset=utf8mb4"
         )
+
+
+db_config = DBConfig()
