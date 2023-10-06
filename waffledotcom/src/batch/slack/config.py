6f965da2986934f184c626582474c109d249693e
shinhong-park <shinhong.park@linecorp.com>
2023-10-06 20:06:28 +0900

    Perform migration

--- waffledotcom/src/batch/slack/config.py
+++ waffledotcom/src/batch/slack/config.py
@@ -1,14 +1,18 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 from waffledotcom.src.secrets import AWSSecretManager
+from waffledotcom.src.settings import settings
 
 
 class SlackConfig(BaseSettings):
     token: str = ""
 
-    def __init__(self) -> None:
-        super().__init__()
+    model_config = SettingsConfigDict(
+        case_sensitive=False, env_prefix="SLACK_", env_file=settings.env_files
+    )
 
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
         aws_secret = AWSSecretManager()
         if aws_secret.is_available():
             self.token = aws_secret.get_secret("slack_token")
---
+++ waffledotcom/src/database/__init__.py
@@ -0,0 +1,2 @@
+import waffledotcom.src.apps.team.models  # noqa
+import waffledotcom.src.apps.user.models  # noqa
