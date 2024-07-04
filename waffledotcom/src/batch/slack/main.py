9cc75720424eb1cbec7cb13c4268e1af30587ea3
Minkyu Lee <whizkyu@snu.ac.kr>
2024-07-05 01:49:50 +0900

    feat: slack api 별도 서비스로 분리

--- waffledotcom/src/batch/slack/main.py
+++ waffledotcom/src/batch/slack/main.py
@@ -2,40 +2,17 @@ import asyncio
 
 from fastapi import Depends
 from loguru import logger
-from slack_sdk import WebClient
-from slack_sdk.errors import SlackApiError
 
 from waffledotcom.src.apps.user.services import UserService
-from waffledotcom.src.batch.slack.config import slack_config
-from waffledotcom.src.batch.slack.schema import SlackMember
+from waffledotcom.src.batch.slack.services import AsyncSlackApiService
 from waffledotcom.src.utils.dependency_solver import DependencySolver
 
 
-async def create_users_from_slack(user_service: UserService = Depends()):
-    client = WebClient(token=slack_config.token)
-    data = client.users_list().data
-
-    assert isinstance(data, dict)
-
-    if not data.get("ok", False):
-        raise SlackApiError("Slack API Error", data)
-
-    members_to_create = []
-    for member in data.get("members", []):
-        if member["is_bot"] or member["deleted"] or member["id"] == "USLACKBOT":
-            continue
-
-        member = SlackMember(**member)
-        if member.profile.phone is not None:
-            phone = (
-                member.profile.phone.replace("-", "")
-                .replace(" ", "")
-                .replace("+82", "")
-            )
-            member.profile.phone = phone
-
-        members_to_create.append(member)
-
+async def create_users_from_slack(
+    user_service: UserService = Depends(),
+    slack_api_service: AsyncSlackApiService = Depends(),
+):
+    members_to_create = await slack_api_service.get_members()
     user_service.create_users_from_slack(members_to_create)
     logger.debug(f"Created {len(members_to_create)} users from slack")
 
