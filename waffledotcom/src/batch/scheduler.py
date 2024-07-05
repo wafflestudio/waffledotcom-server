147025738425adcb265b3fc18fac649388332dbb
MINKYU LEE <whizkyu@snu.ac.kr>
2024-07-05 14:59:12 +0900

    fix: 스케줄링 체크 주기

--- waffledotcom/src/batch/scheduler.py
+++ waffledotcom/src/batch/scheduler.py
@@ -13,10 +13,10 @@ from waffledotcom.src.settings import settings
 async def schedule_tasks():
     scheduler = Scheduler()
     if settings.is_dev:
-        scheduler.every().day.at("00:00", "Asia/Seoul").do(slack_main)
+        scheduler.every().saturday.at("00:00", "Asia/Seoul").do(slack_main)
     if settings.is_prod:
         scheduler.every().sunday.at("00:00", "Asia/Seoul").do(slack_main)
     while True:
         # 최소 주기를 60초로 설정
-        await asyncio.sleep(10)
+        await asyncio.sleep(60)
         scheduler.run_pending()
